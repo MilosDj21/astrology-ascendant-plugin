d645e4e39b9a618d0d89343dfea1e5eb81292545
Milos <m.djuka@hotmail.com>
2024-10-02 12:40:13 +0200

    add cities with coordinates to choose from

--- src/modules/App.js
+++ src/modules/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const App = () => {
   const startDate = new Date();
   const [isValidDate, setIsValidDate] = useState(true);
+  const [coordinates, setCoordinates] = useState("");
   const [birthData, setBirthData] = useState({
     year: startDate.getFullYear(),
     month: startDate.getMonth() + 1,
@@ -85,6 +86,7 @@ const App = () => {
 
   const calculateAscendantSign = () => {
     setAscendant("");
+    console.log(birthData);
 
     const { year, month, day, hour, minute, second, latitude, longitude } = birthData;
 
@@ -182,13 +184,79 @@ const App = () => {
       </div>
 
       <div className="inputDiv">
-        <label>Država: </label>
-        <select>
-          <option value="sel">Srbija</option>
-          <option value="sel">BIH</option>
-          <option value="sel">Crna Gora</option>
-          <option value="sel">Hrvatska</option>
-          <option value="sel">Makedonija</option>
+        <label>Grad: </label>
+        <select
+          value={coordinates}
+          onChange={(e) => {
+            const coordSplit = e.target.value.split("|");
+            setCoordinates(e.target.value);
+            setBirthData({ ...birthData, latitude: Number(coordSplit[0]), longitude: Number(coordSplit[1]) });
+          }}>
+          <option disabled>Srbija</option>
+          <option selected value="44.8125|20.4612">
+            Beograd
+          </option>
+          <option value="45.2396|19.8227">Novi Sad</option>
+          <option value="43.3209|21.8954">Niš</option>
+          <option value="42.6629|21.1655">Priština</option>
+          <option value="44.0128|20.9114">Kragujevac</option>
+          <option value="46.0970|19.6576">Subotica</option>
+          <option value="42.9963|21.9430">Leskovac</option>
+          <option value="43.5787|21.3357">Kruševac</option>
+          <option value="43.7238|20.6873">Kraljevo</option>
+          <option value="45.3834|20.3906">Zrenjanin</option>
+          <option value="44.8706|20.6480">Pančevo</option>
+          <option value="43.8914|20.3506">Čačak</option>
+          <option value="44.7553|19.6923">Šabac</option>
+          <option value="43.1407|20.5181">Novi Pazar</option>
+          <option value="44.6620|20.9302">Smederevo</option>
+          <option value="44.2742|19.8837">Valjevo</option>
+          <option value="42.5521|21.8989">Vranje</option>
+          <option value="45.7733|19.1151">Sombor</option>
+          <option value="44.9798|19.6102">Sremska Mitrovica</option>
+          <option value="44.5338|19.2238">Loznica</option>
+          <option value="43.8556|19.8425">Užice</option>
+          <option disabled>BIH</option>
+          <option value="43.8563|18.4131">Sarajevo</option>
+          <option value="44.7722|17.1910">Banja Luka</option>
+          <option value="44.5375|18.6735">Tuzla</option>
+          <option value="44.2034|17.9077">Zenica</option>
+          <option value="44.7570|19.2150">Bijeljina</option>
+          <option value="43.3438|17.8078">Mostar</option>
+          <option value="44.9778|16.7061">Prijedor</option>
+          <option value="44.8727|18.8106">Brčko</option>
+          <option value="44.7349|18.0843">Doboj</option>
+          <option value="44.3865|19.1048">Zvornik</option>
+          <option disabled>Crna Gora</option>
+          <option value="42.4304|19.2594">Podgorica</option>
+          <option value="42.7805|18.9562">Nikšić</option>
+          <option value="43.3582|19.3513">Pljevlja</option>
+          <option value="43.0369|19.7562">Bijelo Polje</option>
+          <option value="42.3931|18.9116">Cetinje</option>
+          <option value="42.0999|19.1000">Bar</option>
+          <option value="42.4572|18.5315">Herceg Novi</option>
+          <option value="42.8379|19.8604">Berane</option>
+          <option value="42.2911|18.8403">Budva</option>
+          <option value="42.4318|18.6990">Tivat</option>
+          <option disabled>Hrvatska</option>
+          <option value="45.8150|15.9819">Zagreb</option>
+          <option value="43.5147|16.4435">Split</option>
+          <option value="45.3271|14.4422">Rijeka</option>
+          <option value="45.5550|18.6955">Osijek</option>
+          <option value="44.1194|15.2314">Zadar</option>
+          <option value="45.4929|15.5553">Karlovac</option>
+          <option value="44.8666|13.8496">Pula</option>
+          <option value="45.1631|18.0116">Slavonski Brod</option>
+          <option value="42.6507|18.0944">Dubrovnik</option>
+          <option value="45.0812|13.6387">Rovinj</option>
+          <option disabled>Makedonija</option>
+          <option value="41.9981|21.4254">Skoplje</option>
+          <option value="41.0297|21.3292">Bitola</option>
+          <option value="42.1333|21.7258">Kumanovo</option>
+          <option value="41.3441|21.5528">Prilep</option>
+          <option value="41.7165|21.7723">Veles</option>
+          <option value="41.7464|22.1997">Štip</option>
+          <option value="41.1231|20.8016">Ohrid</option>
         </select>
       </div>
 
